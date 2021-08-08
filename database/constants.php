d9d480b40091c8c50bab9d0382c9c813254b820a
ShivG <83803677+shivu0208@users.noreply.github.com>
2021-08-08 09:08:36 +0530

    Add files via upload

---
+++ database/constants.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+define("HOST","localhost:3307");
+define("USER","root");
+define("PASS","");
+define("DB","project_inv");
+
+define("DOMAIN","http://localhost/invproject");
+
+?>
